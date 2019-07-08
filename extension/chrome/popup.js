a8a6d5b2e20abf602184ae3907a901fe17a1cc06
Ibrahim Diallo <ibuvdiallo@gmail.com>
2019-07-07 19:08:41 -0700

    removing boilerplate

--- extension/chrome/popup.js
+++ extension/chrome/popup.js
@@ -1,21 +1,14 @@
-/* global chrome */
+/* global chrome, document, window */
 
-
-var Dupe = {
-	pageUrl:null,
-	init:function(){
+var Gad = {
+	init: function(){
 		var man = chrome.runtime.getManifest();
 		var version = man.version;
 		var vbox = document.getElementById("version-line");
 		vbox.innerText = "Version: "+version;
-		console.log(version);
 	}
 };
 
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
-	Dupe.pageUrl = tabs[0].url;
-});
-
 window.onload = function(){
-	Dupe.init();
+	Gad.init();
 };
